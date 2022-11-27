bb23ce62b426ba248616f1fd36789ad8b381f2b6
willianglitch <nailliw901@gmail.com>
2022-11-26 22:13:27 -0300

    add card componet and figuring out how to organize the components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import { Home } from "./components/pages/Home"
+
 import { NavBar } from "./components/NavBar/Nav"
+import { Box } from "@mui/material"
+import { PokemonCard } from "./components/Card/Card"
+import { Title } from "./components/Title/Title"
 
 
 export function App() {
@@ -7,6 +10,13 @@ export function App() {
   return (
     <div>
       <NavBar />
+      <Box sx={{maxWidth:'1000px', margin:'auto', textAlign:'center', textDecoration:'underline', color:'#55545A'}}>
+        <Title />
+      </Box>
+
+      <Box>
+        <PokemonCard />
+      </Box>
     </div>
   )
 }
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,14 @@
+
+export function Title(){
+
+    // const Link = () {
+    //     return(
+    //         <a href="https://scarletviolet.pokemon.com/en-us/">Scarlet and Violet</a>
+    //     )
+    // }
+
+
+    return(
+            <h1>Time Pokémon para os jogos pokemon Scarlet and Violet </h1>
+    )
+}
